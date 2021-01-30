cd24f2426d93e87261527a55b3c5484be4458253
Alberto González <alberto91base@gmail.com>
2021-01-30 20:44:06 +0100

    add config to prod

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,18 +1,31 @@
-const path = require("path");
-const merge = require("webpack-merge");
-const baseConfig = require("./webpack.base.js");
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+const Uglify = require('uglifyjs-webpack-plugin');
 
 const config = {
   // Tell webpack the root file of our
   // server application
-  entry: "./src/client/client.js",
+  entry: './src/client/client.js',
 
   // Tell webpack where to put the output file
   // that is generated
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: process.env.NODE_ENV
+      }
+    })
+  ]
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(new Uglify());
+}
 
 module.exports = merge(baseConfig, config);
